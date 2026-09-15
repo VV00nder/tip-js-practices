4439690e1420ee6114ad43b7170f20001b81abf5
VV00nder <gio.baranikovi@gmail.com>
2026-09-15 11:24:47 +0300

    chore: first exercise

---
+++ practice-01/js/types.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const result_1 = "8" + 2
+console.log("Результат:", result_1);
+console.log("Тип результата:", typeof result_1);
+
+const result_2 = "8" - 2
+console.log("Результат:", result_2);
+console.log("Тип результата:", typeof result_2);
+
+const result_3 = Number("8") + 2
+console.log("Результат:", result_3);
+console.log("Тип результата:", typeof result_3);
+
+const result_4 = "12" > "3"
+console.log("Результат:", result_4);
+console.log("Тип результата:", typeof result_4);
+
+const result_5 = 12 === "12"
+console.log("Результат:", result_5);
+console.log("Тип результата:", typeof result_5);
+
+const result_6 = Number("")
+console.log("Результат:", result_6);
+console.log("Тип результата:", typeof result_6);
+
+const result_7 = Number("text")
+console.log("Результат:", result_7);
+console.log("Тип результата:", typeof result_7);
+
+const result_8 = Boolean("false")
+console.log("Результат:", result_8);
+console.log("Тип результата:", typeof result_8);
